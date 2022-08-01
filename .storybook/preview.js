bc1e8ef0fd80c759296f20c9b6eb8570bc21e106
Bradley-Laubscher <bradleylaubscher@gmail.com>
2022-08-01 11:43:54 +0200

    added accessability test "a11y"

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,14 +14,19 @@ export const decorators = [
         <Story />
       </>
     ),
-  ];
+];
 
+  /*
+* More on Storybook global parameters at:
+* https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+*/
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    // Storybook a11y addon configuration
+    a11y: {
+      // the target DOM element
+      element: '#root',
+      // sets the execution mode for the addon
+      manual: false,
     },
-  },
-}
+};
